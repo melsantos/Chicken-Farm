1ca33139e5dc7c75daff1fd9afa7131a03dc1545
Melissa Santos <msant035@ucr.edu>
2019-03-11 10:06:59 -0700

    Starting 1 and 4 mostly done. Teleport script implemented. Changed asset pack

---
+++ Assets/scripts/teleport.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class teleport : MonoBehaviour {
+
+    public GameObject teleportTo;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        other.transform.position = teleportTo.transform.position;
+    }
+}
